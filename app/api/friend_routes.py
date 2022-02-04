c5ba04a86425dcb7331c158e2a2f43a920adeb17
Vivian Thach <vivianlthach@gmail.com>
2022-02-04 15:05:54 -0800

    Implement get_friends route to get all friends of the current_user

--- app/api/friend_routes.py
+++ app/api/friend_routes.py
@@ -3,3 +3,9 @@ from app.models import db, User, Friend
 from flask_login import current_user, login_required
 
 friend_routes = Blueprint('friend', __name__)
+
+@friend_routes.route('/')
+@login_required
+def get_friends():
+    friends = Friend.query.filter(Friend.user_id == current_user.id).all()
+    return { "friends": [friend.to_dict() for friend in friends] }
