eb8b1dbacc12a9d2f0740e069c1b92c253237ea4
Cody Lavene <85128425+codylavene@users.noreply.github.com>
2022-02-04 15:11:34 -0600

    frontend setup for avoiding overlap and merge issues

---
+++ react-app/src/components/BillsTab/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Comments from "./Comments";
+
+const BillsTab = (props) => {
+	return (
+		<div>
+			<div>Bills Tab</div>
+			<Comments />
+		</div>
+	);
+};
+
+export default BillsTab;
---
+++ react-app/src/components/FriendsTab/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FriendsTab = (props) => {
+	return (
+		<div>
+			<div>FriendsTab</div>
+		</div>
+	);
+};
+
+export default FriendsTab;
