ac850cb02c399a33e8e1efa1912b1b0cdef1eeac
Vivian Thach <vivianlthach@gmail.com>
2022-02-04 15:12:23 -0800

    Create friends reducer and combined it with the root reducer

---
+++ react-app/src/store/friends.js
@@ -0,0 +1,9 @@
+
+const initialState = { byId: {}, allIds: [] }
+
+export default function reducer(state = initialState, action){
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import friends from './friends'
 
 const rootReducer = combineReducers({
   session,
+  friends
 });
 
 
