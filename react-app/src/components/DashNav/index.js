db6a9d54ca5df002166a367dc8c0d9ef1da9e299
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-06 12:29:20 -0800

    active css for nav tabs, needs adjusting but its there

--- react-app/src/components/DashNav/index.js
+++ react-app/src/components/DashNav/index.js
@@ -5,13 +5,13 @@ const DashNav = (props) => {
 		<div className='dash-nav-container'>
 			<div className='all-tabs-container'>
 				<div className='tab-container'>
-					<NavLink to="/">Bills</NavLink>
+					<NavLink activeClassName="active-tab" exact to="/">Bills</NavLink>
 				</div>
 				<div className='tab-container'>
-					<NavLink to="/friends">Friends</NavLink>
+					<NavLink activeClassName="active-tab" to="/friends">Friends</NavLink>
 				</div>
 				<div className='tab-container'>
-					<NavLink to="/transaction-history">Transactions</NavLink>
+					<NavLink activeClassName="active-tab" to="/transaction-history">Transactions</NavLink>
 				</div>
 			</div>
 		</div>
