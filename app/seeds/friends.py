23a87257aa1e845f51a59edadb5416948750ad5d
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-06 15:15:33 -0800

    a couple more seeders

--- app/seeds/friends.py
+++ app/seeds/friends.py
@@ -10,12 +10,23 @@ def seed_friends():
     friend14 = Friend(user_id=1, friend_id=4)
     friend41 = Friend(user_id=4, friend_id=1)
 
+    friend42 = Friend(user_id=4, friend_id=2)
+    friend24 = Friend(user_id=2, friend_id=4)
+
+    friend43 = Friend(user_id=4, friend_id=3)
+    friend34 = Friend(user_id=3, friend_id=4)
+
+
     db.session.add(friend12)
     db.session.add(friend21)
     db.session.add(friend13)
     db.session.add(friend31)
     db.session.add(friend14)
     db.session.add(friend41)
+    db.session.add(friend42)
+    db.session.add(friend24)
+    db.session.add(friend43)
+    db.session.add(friend34)
 
     db.session.commit()
 
