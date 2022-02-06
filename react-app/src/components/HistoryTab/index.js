dcd634f7fc28ad14a9fd382dde0ffb5bb13d1ef5
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-06 11:25:52 -0800

    transaction container name

--- react-app/src/components/HistoryTab/index.js
+++ react-app/src/components/HistoryTab/index.js
@@ -3,7 +3,7 @@ import TransactionRecords from "./TransactionRecords";
 
 const HistoryTab = (props) => {
 	return (
-		<div>
+		<div className='main-container'>
 			<h2>Transaction History</h2>
 			<TransactionRecords />
 		</div>
