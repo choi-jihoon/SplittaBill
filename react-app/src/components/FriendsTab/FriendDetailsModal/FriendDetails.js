ba27f248072981e97b788ddafe63daffc39352c6
Vivian Thach <vivianlthach@gmail.com>
2022-02-07 15:39:32 -0800

    Add message in friend details modal if there are no transaction records

--- react-app/src/components/FriendsTab/FriendDetailsModal/FriendDetails.js
+++ react-app/src/components/FriendsTab/FriendDetailsModal/FriendDetails.js
@@ -18,6 +18,7 @@ const FriendDetails = ({ showModal, friendId, username, balance }) => {
             <h3>{username} Details</h3>
             {balance !== 0 ? <p>Current Balance: {balance}</p> : <p>All settled up!</p>}
             <h4>Transaction History with {username}</h4>
+            {records.length === 0 ? <p>Nothing to see here yet!</p> : null}
             {records?.map(record => {
                 return <TransactionRecordDetail
                     key={record.id}
