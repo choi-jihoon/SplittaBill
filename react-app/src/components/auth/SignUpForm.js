85e19e552470156c980ec1b979d28c046c4cc944
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-07 09:10:11 -0800

    demo user login

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { signUp } from "../../store/session";
+import DemoLogin from "./DemoLogin";
 import "./LoginForm.css";
 const SignUpForm = () => {
 	const [errors, setErrors] = useState([]);
@@ -84,6 +85,7 @@ const SignUpForm = () => {
 			></input>
 
 			<button type="submit">Sign Up</button>
+			<DemoLogin />
 		</form>
 	);
 };
