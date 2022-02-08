c48b74e6cacff5a46943ccd31acbefbc73b4a68f
Cody Lavene <85128425+codylavene@users.noreply.github.com>
2022-02-08 11:22:13 -0600

    test color-schemes

--- react-app/src/components/DashNav/index.js
+++ react-app/src/components/DashNav/index.js
@@ -2,16 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 const DashNav = (props) => {
 	return (
-		<div className='dash-nav-container'>
-			<div className='all-tabs-container'>
-				<div className='tab-container'>
-					<NavLink activeClassName="active-tab" exact to="/">Bills</NavLink>
+		<div className="dash-nav-container">
+			<div className="all-tabs-container">
+				<div className="tab-container">
+					<NavLink activeClassName="active-tab" exact to="/">
+						<i className="fas fa-user"></i>
+					</NavLink>
 				</div>
-				<div className='tab-container'>
-					<NavLink activeClassName="active-tab" to="/friends">Friends</NavLink>
+				<div className="tab-container">
+					<NavLink activeClassName="active-tab" to="/friends">
+						<i className="fas fa-user-friends"></i>
+					</NavLink>
 				</div>
-				<div className='tab-container'>
-					<NavLink activeClassName="active-tab" to="/transaction-history">Transactions</NavLink>
+				<div className="tab-container">
+					<NavLink
+						activeClassName="active-tab"
+						to="/transaction-history"
+					>
+						<i className="fas fa-history"></i>
+					</NavLink>
 				</div>
 			</div>
 		</div>
