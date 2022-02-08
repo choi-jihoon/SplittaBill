282909631f4f4b7791837b8dafccaec6af183fc7
Vivian Thach <vivianlthach@gmail.com>
2022-02-08 13:21:05 -0800

    Change font-size on friend info

--- react-app/src/components/FriendsTab/Friend/index.js
+++ react-app/src/components/FriendsTab/Friend/index.js
@@ -26,7 +26,7 @@ const Friend = ({id, friendId, image, username, balance}) => {
 				</div>
 				<div className="friend-info">
 					{balance > 0 ?
-						<h3><span className="bold">{username}</span> owes you <span className="positive-payment">${balance}</span></h3>
+						<h3><span className="bold">{username}</span> owes you <span className="positive-payment ">${balance}</span></h3>
 						: (balance < 0 ?
 						<h3>you owe <span className="bold">{username}</span> <span className="negative-payment">${Math.abs(balance).toFixed(2)}</span></h3>
 						:  <h3>All Even with <span className="bold">{username}</span>!</h3>)
