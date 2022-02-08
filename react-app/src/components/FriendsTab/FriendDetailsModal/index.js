e62265cf63e7328767f400dee908bc9fc3385dfe
Vivian Thach <vivianlthach@gmail.com>
2022-02-08 12:53:23 -0800

    Fix styling on records for friend details modal

--- react-app/src/components/FriendsTab/FriendDetailsModal/index.js
+++ react-app/src/components/FriendsTab/FriendDetailsModal/index.js
@@ -30,10 +30,13 @@ const FriendDetails = ({ showModal, friendId, username, balance, image }) => {
                 <h4>Transaction History with {username}</h4>
                 {records.length === 0 ? <p>Nothing to see here yet!</p> : null}
                 {records?.map(record => {
-                    return <TransactionRecordDetail
-                        key={record.id}
-                        record={record}
-                    />
+                    return (<div className="friend-detail-record">
+                        <TransactionRecordDetail
+                            key={record.id}
+                            record={record}
+                        />
+
+                    </div>)
                 })}
             </div>
 
