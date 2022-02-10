1487cfd167c0b8042ef9b3c57b6b4ff9e74f0c55
Vivian Thach <vivianlthach@gmail.com>
2022-02-10 11:14:35 -0800

    Add styles to about links on dashboard, change sidebar to fixed and main container to scroll

--- react-app/src/components/SidePanel/AboutLinks/index.js
+++ react-app/src/components/SidePanel/AboutLinks/index.js
@@ -3,63 +3,72 @@ import "./AboutLinks.css";
 const AboutLinks = () => {
 	return (
 		<div className="side-panel-about-container">
-			<h4>Meet The Developers</h4>
-			<div className="">
-                <div className="">
-					Cody Lavene
+			<h3>Meet The Developers</h3>
+			<div className="about-profiles">
+                <div className="side-panel-about-profile">
+                    <img src="http://splitabill.s3.amazonaws.com/1d5fa99297fa407e94f293f5ad0e40ff.png" alt="cody-pic"></img>
 					<div className="links">
-						<a
-							href="https://github.com/codylavene"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<i className="fab fa-github"></i>
-						</a>
-						<a
-							href="https://www.linkedin.com/in/codylavene/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<i className="fab fa-linkedin"></i>
-						</a>
+					    <p>Cody Lavene</p>
+                        <div className="about-icons">
+                            <a
+                                href="https://github.com/codylavene"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/codylavene/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                        </div>
 					</div>
 				</div>
-				<div className="">
-					Fiona Choi
+				<div className="side-panel-about-profile">
+                <img src="http://splitabill.s3.amazonaws.com/5291b74b486d48b19223f3aab8097bd4.jpg" alt="fiona-pic"></img>
 					<div className="links">
-						<a
-							href="https://github.com/choi-jihoon"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<i className="fab fa-github"></i>
-						</a>
-						<a
-							href="https://www.linkedin.com/in/jihoon-choi-a6967a221/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<i className="fab fa-linkedin"></i>
-						</a>
+					    <p>Fiona Choi</p>
+                        <div className="about-icons">
+                            <a
+                                href="https://github.com/choi-jihoon"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/jihoon-choi-a6967a221/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                        </div>
 					</div>
 				</div>
-				<div className="">
-					Vivian Thach
+				<div className="side-panel-about-profile">
+                    <img src="http://splitabill.s3.amazonaws.com/f2f1be08ffc9479ca754ad89a3b6f85d.jpg" alt="vivian-pic"></img>
 					<div className="links">
-						<a
-							href="https://github.com/bobaguardian"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<i className="fab fa-github"></i>
-						</a>
-						<a
-							href="https://www.linkedin.com/in/vivianthach1023/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<i className="fab fa-linkedin"></i>
-						</a>
+                        <p>Vivian Thach</p>
+                        <div className="about-icons">
+                            <a
+                                href="https://github.com/bobaguardian"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <i className="fab fa-github"></i>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/vivianthach1023/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                        </div>
 					</div>
 				</div>
 			</div>
