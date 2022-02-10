b94524625897ac0be78fcb96f1dfb1991cb3a8a8
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-10 12:22:16 -0800

    empty tab class name adjusted

--- react-app/src/components/BillsTab/EmptyBillsTab/index.js
+++ react-app/src/components/BillsTab/EmptyBillsTab/index.js
@@ -2,7 +2,7 @@ import "./EmptyBillsTab.css";
 
 const EmptyBillsTab = () => {
     return (
-        <div className='empty-bills-tab-container'>
+        <div className='empty-tab-container'>
             <div className='empty-bills-duck'>
             </div>
             <h2>No bills to display, other than the one on my face!</h2>
--- react-app/src/components/HistoryTab/EmptyTransactions/index.js
+++ react-app/src/components/HistoryTab/EmptyTransactions/index.js
@@ -2,7 +2,7 @@ import "./EmptyTransactions.css";
 
 const EmptyTransactionsTab = () => {
     return (
-        <div className='empty-bills-tab-container'>
+        <div className='empty-tab-container'>
             <div className='empty-transactions-mallard'>
             </div>
             <h2>No transQUACKtions have been made yet!</h2>
